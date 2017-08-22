4324b96729e7b0070e791c8d8d8cb5252d99d17c
David Cheung <cheung.david@hotmail.ca>
2017-08-21 22:06:48 -0400

    Optimized bundle and re-added Google Play links

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -28,6 +28,10 @@ class Feature extends Component {
                             <div className="intro-lead-in">Track And Share Experiences From Around The World</div>
                             <a href="#services" className="btn btn-xl" data-toggle="modal" data-target=".login-modal">Join Now!</a>
                             <button type="button" className="btn btn-default btn-xl" onClick={this.handleGuest.bind(this)}>Preview as Guest</button>
+                            <div>
+                                <a className="intro-download" href='https://play.google.com/store/apps/details?id=com.dc.scribe&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                                <img alt='Get it on GooglePlay' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+                            </div>
                         </div>
                     </div>
                     
@@ -70,7 +74,7 @@ class Feature extends Component {
                                 <i className="fa fa fa-android fa-stack-1x fa-inverse"></i>
                             </span>
                             <h4 className="service-heading">Android</h4>
-                            <p className="text-muted">Currently only supports Android and Web. Sorry iPhone is not currently supported, stay tuned!</p>
+                            <p className="text-muted">Currently only supports Android and Web. Sorry iPhone is not currently supported, stay tuned! <br/><a href="https://play.google.com/store/apps/details?id=com.dc.scribe">Download On GooglePlay!</a></p>
                         </div>
                         <div className="col-md-4">
                             <span className="fa-stack fa-4x">
@@ -86,7 +90,7 @@ class Feature extends Component {
                                 <i className="fa fa-code fa-stack-1x fa-inverse"></i>
                             </span>
                             <h4 className="service-heading">Updates</h4>
-                            <p className="text-muted">This is just the beginning of the journey. New and cool features still rolling in!</p>
+                            <p className="text-muted">We are currently in Beta. This is just the beginning of the journey. New and cool features still rolling in!</p>
                         </div>
                     </div>
                 </div>
